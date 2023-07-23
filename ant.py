f391aa8bbb5f07fa12c3b2b454eb71d412bd4dee
emmanuelraob <emmanuelraob123@gmail.com>
2023-07-22 22:05:30 -0600

    Ant changes
    Ants now last in time, change color when loosing life, die if not have life

--- ant.py
+++ ant.py
@@ -4,16 +4,16 @@ import random
 import pygame
 
 WIN_WIDTH, WIN_HEIGHT = 800, 600
+HEALTH, FOOD = 30*30, 30*120 #30*30, #30*120
 ANT_RADIUS = 2
-SPEED = 1
 
 class Ant:
     def __init__(self, id, x=WIN_WIDTH/2 , y=WIN_HEIGHT/2 ):
         self.id = id
         self.x = x
         self.y = y
-        self.health = 100
-        self.food = 100
+        self.health = HEALTH  # this amount of heath last for 30 seconds before the ant die
+        self.food = FOOD # this amount of food last arround 2 minutes
         self.speed = random.uniform(0.5,1.5)
         self.angle = random.uniform(0, 2*math.pi)  # ants start with a random orientation
         self.color = (0,0,0)
@@ -36,27 +36,25 @@ class Ant:
 
         # If the ant hits the left or right edge of the screen, adjust its angle by π plus a random amount
         if self.x < ANT_RADIUS or self.x > WIN_WIDTH - ANT_RADIUS:
-            self.angle = math.pi - self.angle + random.uniform(-1, -1)
+            self.angle = math.pi - self.angle + random.uniform(-0.5, -0.5)
 
         # If the ant hits the top or bottom edge of the screen, flip the sign of its angle and add a random amount
         if self.y < ANT_RADIUS or self.y > WIN_HEIGHT - ANT_RADIUS:
-            self.angle = -self.angle + random.uniform(-1, 1)
+            self.angle = -self.angle + random.uniform(-0.5, 0.5)
 
 
     def eat(self, meal):
         if self.food >=0 and meal<0:
             self.food += meal
-        elif self.food < 100 and meal>0:
+        elif self.food < FOOD and meal>0:
             self.food += meal
-            if self.food >100:
-                self.food = 100
+            if self.food >FOOD:
+                self.food = FOOD
     
     def life(self):
         if self.food<=0:
-            self.health =- 1
+            self.health -= 1
     
     def view_health(self):
-        if self.health <= 0:
+        if self.food <= 0:
             self.color = (255,0,0)
-        else:
-            self.color = (0,0,0)
--- colony.py
+++ colony.py
@@ -6,29 +6,42 @@ class Colony:
         self.size = size
         self.x = 100
         self.y = 100
-        self.ants = [Ant(i) for i in range(size)]
+        self.ants = {i: Ant(i) for i in range(size)}
         self.food = 1000
 
     def draw(self, win):
-        for ant in self.ants:
+        for ant in self.ants.values():
             ant.draw(win)
     
     def move_ants(self):
-        for ant in self.ants:
+        for ant in self.ants.values():
             ant.move()
 
     def eat_once(self):
-        for ant in self.ants:
+        for ant in self.ants.values():
             ant.eat(-3)
 
     def eat_full(self):
-        for ant in self.ants:
-            ant.eat(100)
+        for ant in self.ants.values():
+            ant.eat(1000)
 
     def view_health(self):
-        for ant in self.ants:
+        for ant_id, ant in list(self.ants.items()):
             ant.view_health()
+            if ant.health <= 0:
+                self.remove_ant(ant_id)
     
     def life(self):
-        for ant in self.ants:
+        for ant in self.ants.values():
             ant.life()
+
+    
+    def add_ant(self):
+        new_id = self.size
+        self.size += 1
+        self.ants[new_id] = Ant(new_id)
+
+    def remove_ant(self, ant_id):
+        if ant_id in self.ants:
+            del self.ants[ant_id]
+            self.size -= 1
